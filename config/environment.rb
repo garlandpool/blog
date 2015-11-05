273e1b10aad8dd482c3a82c590f1acf6421c1bbf
garlandpool <frederic.pool@threerivers.k12.or.us>
2015-11-05 11:43:35 -0800

    Initial Commit

---
+++ config/environment.rb
@@ -0,0 +1,7 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+Rails.application.initialize!
+
+#ENV['GEM_PATH'] = '/path/to/your/home/ruby/gems:/lib/ruby/gems/1.9.3'
